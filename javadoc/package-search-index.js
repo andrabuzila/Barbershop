2ae10ce9914bee3c8dedafb80f778e47c1d639c6
andrabuzila <72390634+andrabuzila@users.noreply.github.com>
2022-03-25 11:16:53 +0200

    Add files via upload

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.projectps"},{"l":"com.example.projectps.model"},{"l":"com.example.projectps.repository"},{"l":"com.example.projectps.service"}]
